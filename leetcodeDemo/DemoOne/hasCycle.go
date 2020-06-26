149bcb3182ce44cbb7656f0d3393ce22fa999775
wangweicheng <wangweicheng@bilibili.com>
2020-06-26 17:47:52 +0800

    算法学习：循环链表判断

--- leetcodeDemo/DemoOne/hasCycle.go
+++ leetcodeDemo/DemoOne/hasCycle.go
@@ -1,5 +1,35 @@
 package DemoOne
 
-func hasCycle(head *ListNode) bool {
+/*
+141
+快慢指针法
+*/
 
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	slow := head
+	fast := head.Next
+	for slow != fast {
+		if fast == nil || fast.Next == nil {
+			return false
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return true
+}
+
+// hashMap方法
+func hashMapCycle(head *ListNode) bool {
+	hash := make(map[*ListNode]int)
+	for head != nil {
+		if _, ok := hash[head]; ok {
+			return true
+		}
+		hash[head] = head.Val
+		head = head.Next
+	}
+	return false
 }
